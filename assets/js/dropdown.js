386dcfa5b04bd87d04bbec4fd88adcafdbfdb2d5
louisakai <khangcm24@gmail.com>
2024-04-13 00:44:49 +0700

    update product.html

--- assets/js/dropdown.js
+++ assets/js/dropdown.js
@@ -2,3 +2,19 @@ const toggle = document.querySelector('#button');
 const menu = document.querySelector('.menu-filter');
 
 toggle.addEventListener("click", ()=> menu.classList.toggle("active"));
+
+function traicay_redirect() {
+    window.location.href = "traicay.html";
+}
+
+function cu_redirect() {
+    window.location.href = "cu.html";
+}
+
+function rau_redirect() {
+    window.location.href = "rau.html";
+}
+
+function nam_redirect() {
+    window.location.href = "nam.html";
+}
