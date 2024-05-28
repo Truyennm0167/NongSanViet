1ed6ad3ea979d41fb0d606970426f43ae28b1224
Tmh3101 <minhhieu31012004@gmail.com>
2024-05-28 21:57:18 +0700

    edit productpage

--- assets/js/main.js
+++ assets/js/main.js
@@ -126,89 +126,185 @@ const giohang =[
 ];
 
 const products = [
-  { name: 'Ổi trân châu ruột đỏ (1kg)', category: 'trái cây', p_price: '31.000đ', discount: '40%', price: '19.000đ', rating: '4/5',   image: 'img/poster_oi.jpg' },
-  { name: 'Xoài keo (2-3 trái)',        category: 'trái cây', p_price: '25.000đ', discount: '28%', price: '18.000đ', rating: '4.5/5', image: 'img/poster_xoai.jpg'},
-  { name: 'Dưa hấu đỏ (1.8kg)',         category: 'trái cây', p_price: '38.000đ', discount: '11%', price: '34.000đ', rating: '5/5',   image: 'img/poster_duahau.jpg'},
-  { name: 'Chuối già Nam Mỹ (1kg)',     category: 'trái cây', p_price: '33.000đ', discount: '21%', price: '26.000đ', rating: '3.5/5', image: 'img/poster_chuoi.jpg'},
-  { name: 'Cam sành túi (0.9-1.1kg)',   category: 'trái cây', p_price: '25.000đ', discount: '20%', price: '21.000đ', rating: '4/5',   image: 'img/poster_cam.jpg'},
+  { id: "TC001", name: 'Ổi trân châu ruột đỏ (1kg)', category: 'trái cây', p_price: '31.000đ', discount: '40%', price: '19.000đ', rating: '4/5',   image: 'img/poster_oi.jpg' },
+  { id: "TC002", name: 'Xoài keo (2-3 trái)',        category: 'trái cây', p_price: '25.000đ', discount: '28%', price: '18.000đ', rating: '4.5/5', image: 'img/poster_xoai.jpg'},
+  { id: "TC003", name: 'Dưa hấu đỏ (1.8kg)',         category: 'trái cây', p_price: '38.000đ', discount: '11%', price: '34.000đ', rating: '5/5',   image: 'img/poster_duahau.jpg'},
+  { id: "TC004", name: 'Chuối già Nam Mỹ (1kg)',     category: 'trái cây', p_price: '33.000đ', discount: '21%', price: '26.000đ', rating: '3.5/5', image: 'img/poster_chuoi.jpg'},
+  { id: "TC005", name: 'Cam sành túi (0.9-1.1kg)',   category: 'trái cây', p_price: '25.000đ', discount: '20%', price: '21.000đ', rating: '4/5',   image: 'img/poster_cam.jpg'},
   
-  { name: 'Rau muống (500g)',           category: 'rau lá',   p_price: '14.000đ', discount: '12%', price: '12.000đ', rating: '4.5/5', image: 'img/poster_raumuong.jpg'},
-  { name: 'Cải ngọt (500g)',            category: 'rau lá',   p_price: '15.000đ', discount: '32%', price: '10.000đ', rating: '4/5',   image: 'img/poster_caingot.jpg'},
-  { name: 'Mồng tơi (500g)',            category: 'rau lá',   p_price: '15.000đ', discount: '33%', price: '10.000đ', rating: '5/5',   image: 'img/poster_mongtoi.jpg'},
-  { name: 'Xà lách (500g)',             category: 'rau lá',   p_price: '16.000đ', discount: '20%', price: '13.000đ', rating: '4/5',   image: 'img/poster_xalach.jpg'},
-  { name: 'Hành lá (100g)',             category: 'rau lá',   p_price: '7.000đ',  discount: '5%',  price: '7.000đ',  rating: '4/5',   image: 'img/poster_hanhla.jpg'},
+  { id: "RL001", name: 'Rau muống (500g)',           category: 'rau lá',   p_price: '14.000đ', discount: '12%', price: '12.000đ', rating: '4.5/5', image: 'img/poster_raumuong.jpg'},
+  { id: "RL002", name: 'Cải ngọt (500g)',            category: 'rau lá',   p_price: '15.000đ', discount: '32%', price: '10.000đ', rating: '4/5',   image: 'img/poster_caingot.jpg'},
+  { id: "RL003", name: 'Mồng tơi (500g)',            category: 'rau lá',   p_price: '15.000đ', discount: '33%', price: '10.000đ', rating: '5/5',   image: 'img/poster_mongtoi.jpg'},
+  { id: "RL004", name: 'Xà lách (500g)',             category: 'rau lá',   p_price: '16.000đ', discount: '20%', price: '13.000đ', rating: '4/5',   image: 'img/poster_xalach.jpg'},
+  { id: "RL005", name: 'Hành lá (100g)',             category: 'rau lá',   p_price: '7.000đ',  discount: '5%',  price: '7.000đ',  rating: '4/5',   image: 'img/poster_hanhla.jpg'},
 
-  { name: 'Cà rốt (500g)',              category: 'củ quả',   p_price: '10.000đ', discount: '0%',  price: '10.000đ', rating: '3/5',   image: 'img/carot1.jpg'},
-  { name: 'Hành tây (500g)',            category: 'củ quả',   p_price: '18.000đ', discount: '0%',  price: '18.000đ', rating: '4/5',   image: 'img/poster_cuhanhtay.jpg'},
-  { name: 'Khoai tây (500g)',           category: 'củ quả',   p_price: '15.000đ', discount: '0%',  price: '15.000đ', rating: '4.5/5', image: 'img/poster_khoaitay.jpg'},
-  { name: 'Củ cải trắng',               category: 'củ quả',   p_price: '10.000đ', discount: '0%',  price: '10.000đ', rating: '5/5',   image: 'img/poster_cucaitrang.jpg'},
-  { name: 'Su hào (500g)',              category: 'củ quả',   p_price: '16.000đ', discount: '0%',  price: '16.000đ', rating: '5/5',   image: 'img/suhao1.jpg'},
+  { id: "CQ001", name: 'Cà rốt (500g)',              category: 'củ quả',   p_price: '10.000đ', discount: '0%',  price: '10.000đ', rating: '3/5',   image: 'img/carot1.jpg'},
+  { id: "CQ002", name: 'Hành tây (500g)',            category: 'củ quả',   p_price: '18.000đ', discount: '0%',  price: '18.000đ', rating: '4/5',   image: 'img/poster_cuhanhtay.jpg'},
+  { id: "CQ003", name: 'Khoai tây (500g)',           category: 'củ quả',   p_price: '15.000đ', discount: '0%',  price: '15.000đ', rating: '4.5/5', image: 'img/poster_khoaitay.jpg'},
+  { id: "CQ004", name: 'Củ cải trắng',               category: 'củ quả',   p_price: '10.000đ', discount: '0%',  price: '10.000đ', rating: '5/5',   image: 'img/poster_cucaitrang.jpg'},
+  { id: "CQ005", name: 'Su hào (500g)',              category: 'củ quả',   p_price: '16.000đ', discount: '0%',  price: '16.000đ', rating: '5/5',   image: 'img/suhao1.jpg'},
 
-  { name: 'Nấm đùi gà (200g)',          category: 'nấm',      p_price: '31.000đ', discount: '18%', price: '25.000đ', rating: '5/5',   image: 'img/duiga1.jpg' },
-  { name: 'Nấm linh chi (150g)',        category: 'nấm',      p_price: '33.000đ', discount: '0%',  price: '33.000đ', rating: '5/5',   image: 'img/linhchi1.jpg' },
-  { name: 'Nấm hương (150g)',           category: 'nấm',      p_price: '33.000đ', discount: '15%', price: '28.000đ', rating: '4.5/5', image: 'img/namhuong1.jpg' },
-  { name: 'Nấm tuyết Vietfresh (50g)',  category: 'nấm',      p_price: '30.000đ', discount: '0%',  price: '30.000đ', rating: '4/5',   image: 'img/namtuyet1.jpg' },
-  { name: 'Nấm linh chi nâu 150g',      category: 'nấm',      p_price: '33.000đ', discount: '0%',  price: '33.000đ', rating: '3/5',   image: 'img/linhchinau1.jpg' },
+  { id: "N001,", name: 'Nấm đùi gà (200g)',          category: 'nấm',      p_price: '31.000đ', discount: '18%', price: '25.000đ', rating: '5/5',   image: 'img/duiga1.jpg' },
+  { id: "N002,", name: 'Nấm linh chi (150g)',        category: 'nấm',      p_price: '33.000đ', discount: '0%',  price: '33.000đ', rating: '5/5',   image: 'img/linhchi1.jpg' },
+  { id: "N003,", name: 'Nấm hương (150g)',           category: 'nấm',      p_price: '33.000đ', discount: '15%', price: '28.000đ', rating: '4.5/5', image: 'img/namhuong1.jpg' },
+  { id: "N004,", name: 'Nấm tuyết Vietfresh (50g)',  category: 'nấm',      p_price: '30.000đ', discount: '0%',  price: '30.000đ', rating: '4/5',   image: 'img/namtuyet1.jpg' },
+  { id: "N005,", name: 'Nấm linh chi nâu 150g',      category: 'nấm',      p_price: '33.000đ', discount: '0%',  price: '33.000đ', rating: '3/5',   image: 'img/linhchinau1.jpg' },
   // Thêm các sản phẩm khác vào đây
 ];
+
 function searchProduct() {
   const searchInput = document.getElementById('searchInput');
-  var i = 1;
+
+  var searchResults = products;
   if (searchInput && searchInput.value) {
-      const searchKeyword = searchInput.value.toLowerCase();
-      const searchResults = products.filter(product => {
-          return removeDiacritics(product.name.toLowerCase()).includes(removeDiacritics(searchKeyword)) || removeDiacritics(product.category.toLowerCase()).includes(removeDiacritics(searchKeyword));
-      });
+    const searchKeyword = searchInput.value.toLowerCase();
+    searchResults = products.filter(product => {
+      return removeDiacritics(product.name.toLowerCase()).includes(removeDiacritics(searchKeyword)) || removeDiacritics(product.category.toLowerCase()).includes(removeDiacritics(searchKeyword));
+    });
+  } 
 
-      const productList = document.getElementById('productList');
-      productList.innerHTML = ''; // Xóa danh sách sản phẩm hiện tại
+  var i = 1;
+  const productList = document.getElementById('productList');
+  productList.innerHTML = ''; // Xóa danh sách sản phẩm hiện tạ
 
-      if (searchResults.length === 0) {
-          productList.innerHTML = 'Không tìm thấy sản phẩm phù hợp.';
-      } else {
-          searchResults.forEach(product => {
-              const productHtml = `
-                  <div class="col-3 mb-5" style="margin-top:20px;">
-                      <div class="card mt-5" style="margin-left: 30px;">
-                          <img src="${product.image}" alt="${product.name}">
-                          <div class="card-body">
-                              <h5 class="card-title">${product.name}</h5>
-                              <p class="card-text" style="text-decoration:line-through;">${product.p_price}</p>
-                              <div class="discount" style="margin-top: -10px;" style="margin-top: -10px;">
-                                    <p style="display: inline;">${product.price}</p>
-                                    <p style="display: inline; background-color: red; color: white; padding: 2px; align-items: center; margin-left: 3px;">${product.discount}</p>
-                                </div>
-                                <div class="star-rating" style="padding-top: 5px;" style="padding-top: 5px;">
-                                    ${product.rating}<span class="material-symbols-outlined">grade</span>
-                                </div>
-                              <button class="btn btn-success">Thêm vào giỏ hàng</button>
-                          </div>
-                      </div>
-                  </div>
-              `;
-              i += 1;
-              if(i>4){
-                i = 1;
-              }
-              productList.innerHTML += productHtml;
-          });
-      }
+  console.log(searchResults)
+  if (searchResults.length === 0) {
+      productList.innerHTML = 'Không tìm thấy sản phẩm phù hợp.';
   } else {
-      // Xử lý trường hợp không có giá trị nhập vào hoặc không tìm thấy phần tử
-      console.error('Không tìm thấy phần tử input hoặc không có giá trị nhập vào');
+    searchResults.forEach(product => {
+      var productHtml = `
+        <div class="col-3 mb-3" style="margin-top:20px;">
+          <div class="card mt-3" style="margin-left: 30px; cursor: pointer;" onclick="searchProductDetail('${product.id}')">
+            <img src="${product.image}" alt="${product.name}">
+            <div class="card-body">
+              <h5 class="card-title">${product.name}</h5>
+              <p class="card-text" style="text-decoration:line-through;">${product.p_price}</p>
+              <div class="discount" style="margin-top: -10px;" style="margin-top: -10px;">
+                <p style="display: inline;">${product.price}</p>
+                <p style="display: inline; background-color: red; color: white; padding: 2px; align-items: center; margin-left: 3px;">${product.discount}</p>
+              </div>
+              <div class="star-rating" style="padding-top: 5px;" style="padding-top: 5px;">
+                ${product.rating}<span class="material-symbols-outlined">grade</span>
+              </div>
+              <button class="btn btn-success">Thêm vào giỏ hàng</button>
+            </div>
+          </div>
+        </div>
+      `;
+      i += 1;
+      if(i>4){
+        i = 1;
+      }
+      productList.innerHTML += productHtml;
+    });
   }
 }
+
 //bat enter
-function enter_check (enter){
-  if (enter.which == 32 || enter.keyCode == 32){
+function checkEnterKey(event){
+  if (event.code === 'Enter'){
     searchProduct();
   }
 }
 
-
 // Hàm chuyển đổi từ khóa thành dạng không dấu
 function removeDiacritics(str) {
   return str.normalize('NFD').replace(/[\u0300-\u036f]/g, '');
+}
+
+function searchProductDetail(productId){
+  window.location.href = 'product-detail.html?id=' + productId;
+}
+
+function loadProductDetail(productId){
+  const productMain = document.getElementsByClassName('product-main')[0];
+
+  var productDetailInfor = `
+  <div class="card-wrapper" style="margin-top: 30px; margin-bottom: 60px;">
+      <div class="card">
+          <div class="product-imgs">
+              <div class="img-display">
+                  <div class="img-showcase">
+                      <img src="img/cam1.jpg" alt="">
+                      <img src="img/cam2.jpg" alt="">
+                      <img src="img/cam3.jpg" alt="">
+                      <img src="img/cam4.jpg" alt="">
+                  </div>
+              </div>
+
+              <div class="img-select">
+                  <div class="img-item">
+                      <a href="#" data-id="1">
+                          <img src="img/cam1.jpg" alt="">
+                      </a>
+                  </div>
+
+                  <div class="img-item">
+                      <a href="#" data-id="2">
+                          <img src="img/cam2.jpg" alt="">
+                      </a>
+                  </div>
+
+                  <div class="img-item">
+                      <a href="#" data-id="3">
+                          <img src="img/cam3.jpg" alt="">
+                      </a>
+                  </div>
+
+                  <div class="img-item">
+                      <a href="#" data-id="4">
+                          <img src="img/cam4.jpg" alt="">
+                      </a>
+                  </div>
+              </div>
+          </div>
+
+          <!-- Card right -->
+          <div class="product-content">
+              <h2 class="product-title">Cam sành túi (0.9-1.1kg)</h2>
+              <div class="product-rating">
+                  <i class="fa fa-star"></i>
+                  <i class="fa fa-star"></i>
+                  <i class="fa fa-star"></i>
+                  <i class="fa fa-star-half-o"></i>
+                  <i class="fa fa-star-o"></i>
+                  <span>4.2(15)</span>
+              </div>
+
+              <div class="product-price">
+                  <p class="new-price"><span>18.000VNĐ</span></p>
+                  <p>
+                      <span class="last-price">25.000VNĐ</span>
+                      <span class="discount"> 28% </span>
+                  </p>
+              </div>
+
+              <div class="purchase-info">
+                  <input type="number" min="0" value="1">
+                  <button type="button" class="buttn"  onclick="addToCart('Cam sành túi (0.9-1.1kg)','trái cây','img/cam1.jpg',18000)">Thêm vào giỏ hàng <i class="fa fa-shopping-cart"></i>
+                  </button>
+                  <button type="button" class="buttn">Mua
+                  </button>
+              </div>
+
+              <div class="product-detail">
+                  <h2>Về sản phẩm: </h2>
+                  <p>- Ổi trân châu ruột đỏ giòn ngọt, tươi ngon, trái to, không bị dập. Ổi có ruột màu đỏ hồng vô cùng hấp dẫn, ổi ngọt ngon. Ổi ngon, được đóng gói cẩn thận, tiện lợi, là loại trái cây cung cấp nhiều dưỡng chất cho cơ thể. Ổi ngon, giòn nhất khi trái chắc, xanh sáng, cầm nặng tay.</p>
+                  <p>- Sản phẩm sạch, đạt tiêu chuẩn kiểm nghiệm VS ATTP.</p>
+                  <p>- Cam kết hoàn tiền hoặc giao lại ngay sản phẩm trong tất cả trường hợp khách nhận sản phẩm không đạt chất lượng gồm ổi bị dập, úng hoặc hư hỏng bên trong.</p>
+                  <p>- Liên hệ tổng đài 1900.1908 để được hỗ trợ nhanh nhất, hoặc để lại thông tin sản phẩm hư hỏng trên web/app theo hướng dẫn tại đây</p>
+                  <ul>
+                      <li>Trạng Thái: <span>Còn hàng</span></li>
+                      <li>Khu Vực Vận Chuyển: <span>Khắp Việt Nam</span></li>
+                      <li>Phí Vận Chuyển: <span>20.000Đ</span></li>
+                  </ul>
+              </div>
+          </div>
+      </div>
+  </div>
+  `;
+
+  productMain.innerHTML = productDetailInfor;
 }
 
 function cam_redirect() {
